791d37e943aae78a32a8a7026c7d482331d4c370
phibedy <philipp.schmette@gmx.de>
2015-09-16 12:01:07 +0200

    added face-detector, TODO: add framework-config, add cascade-configs

--- external/modules/opencv_face_detector/include/face_detector.h
+++ external/modules/opencv_face_detector/include/face_detector.h
@@ -1,19 +1,32 @@
-#pragma once
 
+// What is that? #pragma once
 #include <lms/datamanager.h>
 #include <lms/module.h>
 #include <lms/type/module_config.h>
 #include <lms/imaging/image.h>
+#include "opencv2/objdetect.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <cstring>
+
 
 namespace lms_opencv {
 
 class FaceDetector : public lms::Module {
 public:
 
+    const lms::imaging::Image input;
+
     bool initialize();
     bool deinitialize();
 
     bool cycle();
+
+    void detectAndDisplay( cv::Mat frame );
+
+
+    void convertImage(cv::Mat output,const lms::imaging::Image input);
 };
 
 }  // namespace lms_opencv
--- external/modules/opencv_face_detector/src/face_detector.cpp
+++ external/modules/opencv_face_detector/src/face_detector.cpp
@@ -2,11 +2,22 @@
 #include "lms/messaging.h"
 
 #include "face_detector.h"
+#include <iostream>
+#include <stdio.h>
+
+/** Global variables */
+std::string face_cascade_name = "haarcascade_frontalface_alt.xml";
+std::string eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+cv::CascadeClassifier face_cascade;
+cv::CascadeClassifier eyes_cascade;
+std::string window_name = "Capture - Face detection";
 
 namespace lms_opencv {
 
 bool FaceDetector::initialize(){
-    //TODO
+    input = datamanager()->readChannel(this,"IMAGE");
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
+    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
     return true;
 }
 
@@ -15,7 +26,58 @@ bool FaceDetector::deinitialize() {
 }
 
 bool FaceDetector::cycle () {
+
+    cv::Mat frame;
+    convertImage(frame,input);
     //TODO convert image to opencv image
+        if( frame.empty() ){
+            logger.info(" --(!) No captured frame!");
+            return true;
+        }else{
+            //-- 3. Apply the classifier to the frame
+            detectAndDisplay( frame );
+        }
     return true;
+}
+
+void FaceDetector::convertImage(cv::Mat output,const lms::imaging::Image input){
+    int width = input.width();
+    int height = input.height();
+
+    output(cv::Size(width,height),CV_8UC1,input.data(),cv::Mat::AUTO_STEP);
+}
+
+/** @function detectAndDisplay */
+void FaceDetector::detectAndDisplay( cv::Mat frame ){
+    using namespace cv;
+    std::vector<Rect> faces;
+    Mat frame_gray;
+
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+
+        Mat faceROI = frame_gray( faces[i] );
+        std::vector<Rect> eyes;
+
+        //-- In each face, detect eyes
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+        for ( size_t j = 0; j < eyes.size(); j++ )
+        {
+            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
+    }
+    //-- Show what you got
+    imshow( window_name, frame );
 }
 }
